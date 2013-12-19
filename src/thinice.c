2d9162422718b563d7366dfa91cf0376fd52cdbf
Félix Arreola Rodríguez <fgatuno.123@gmail.com>
2013-12-19 16:32:46 -0600

    Nuevas imágenes.
    * Nueva máquina de estados para los tiles.
    * Prueba de los tiles.

--- src/thinice.c
+++ src/thinice.c
@@ -51,20 +51,346 @@
 #define TRUE !FALSE
 #endif
 
+#define TILE_HEIGHT 24
+#define TILE_WIDTH 24
+
 /* Enumerar las imágenes */
 enum {
-	/* TODO: Listar las imágenes */
-	IMG_DUMMY,
+	IMG_ARCADE,
+	
+	IMG_DOWN_1,
+	IMG_DOWN_2,
+	IMG_DOWN_3,
+	
+	IMG_LEFT_1,
+	IMG_LEFT_2,
+	IMG_LEFT_3,
+	
+	IMG_RIGHT_1,
+	IMG_RIGHT_2,
+	IMG_RIGHT_3,
+	
+	IMG_UP_1,
+	IMG_UP_2,
+	IMG_UP_3,
 	
 	NUM_IMAGES
 };
 
-const char *images_names[NUM_IMAGES] = {
-	/* TODO: Listar las rutas */
-	GAMEDATA_DIR "images/dummy.png",
+/* Enumerar cada uno de los Tiles */
+enum {
+	TILE_PORTAL_1,
+	TILE_PORTAL_2,
+	TILE_PORTAL_3,
+	TILE_PORTAL_4,
+	TILE_PORTAL_5,
+	TILE_PORTAL_6,
+	TILE_PORTAL_7,
+	TILE_PORTAL_8,
+	
+	TILE_KEY_1,
+	TILE_KEY_2,
+	TILE_KEY_3,
+	TILE_KEY_4,
+	TILE_KEY_5,
+	TILE_KEY_6,
+	TILE_KEY_7,
+	TILE_KEY_8,
+	TILE_KEY_9,
+	TILE_KEY_10,
+	TILE_KEY_11,
+	TILE_KEY_12,
+	TILE_KEY_13,
+	TILE_KEY_14,
+	TILE_KEY_15,
+	TILE_KEY_16,
+	
+	TILE_BAG,
+	TILE_GOAL,
+	TILE_EMPTY,
+	TILE_ICE,
+	TILE_BOXSPACE,
+	TILE_DOBLE,
+	TILE_PORTAL_EMPTY,
+	TILE_LOCK,
+	
+	TILE_SWITCH_A,
+	TILE_SWITCH_B,
+	TILE_WALL,
+	TILE_BLOCK,
+	
+	TILE_LOSE_1,
+	TILE_LOSE_2,
+	TILE_LOSE_3,
+	TILE_LOSE_4,
+	TILE_LOSE_5,
+	TILE_LOSE_6,
+	TILE_LOSE_7,
+	TILE_LOSE_8,
+	TILE_LOSE_9,
+	TILE_LOSE_10,
+	TILE_LOSE_11,
+	TILE_LOSE_12,
+	TILE_LOSE_13,
+	TILE_LOSE_14,
+	TILE_LOSE_15,
+	TILE_LOSE_16,
+	TILE_LOSE_17,
+	TILE_LOSE_18,
+	TILE_LOSE_19,
+	TILE_LOSE_20,
+	
+	TILE_BLACK_NORMAL_1,
+	TILE_BLACK_NORMAL_2,
+	TILE_BLACK_NORMAL_3,
+	TILE_BLACK_NORMAL_4,
+	TILE_BLACK_NORMAL_5,
+	TILE_BLACK_NORMAL_6,
+	TILE_BLACK_NORMAL_7,
+	
+	TILE_BLACK_START_1,
+	TILE_BLACK_START_2,
+	TILE_BLACK_START_3,
+	TILE_BLACK_START_4,
+	TILE_BLACK_START_5,
+	
+	TILE_BLACK_OFF_1,
+	TILE_BLACK_OFF_2,
+	TILE_BLACK_OFF_3,
+	TILE_BLACK_OFF_4,
+	TILE_BLACK_OFF_5,
+	TILE_BLACK_OFF_6,
+	TILE_BLACK_OFF_7,
+	TILE_BLACK_OFF_8,
+	TILE_BLACK_OFF_9,
+	TILE_BLACK_OFF_10,
+	TILE_BLACK_OFF_11,
+	TILE_BLACK_OFF_12,
+	TILE_BLACK_OFF_13,
+	TILE_BLACK_OFF_14,
+	TILE_BLACK_OFF_15,
+	TILE_BLACK_OFF_16,
+	TILE_BLACK_OFF_17,
+	TILE_BLACK_OFF_18,
+	TILE_BLACK_OFF_19,
+	TILE_BLACK_OFF_20,
+	TILE_BLACK_OFF_21,
+	TILE_BLACK_OFF_22,
+	TILE_BLACK_OFF_23,
+	TILE_BLACK_OFF_24,
+	TILE_BLACK_OFF_25,
+	TILE_BLACK_OFF_26,
+	TILE_BLACK_OFF_27,
+	TILE_BLACK_OFF_28,
+	
+	TILE_MELT_1,
+	TILE_MELT_2,
+	TILE_MELT_3,
+	TILE_MELT_4,
+	TILE_MELT_5,
+	TILE_MELT_6,
+	TILE_MELT_7,
+	TILE_MELT_8,
+	TILE_MELT_9,
+	TILE_MELT_10,
+	TILE_MELT_11,
+	TILE_MELT_12,
+	TILE_MELT_13,
+	TILE_MELT_14,
+	TILE_MELT_15,
+	TILE_MELT_16,
+	TILE_MELT_17,
+	TILE_MELT_18,
+	TILE_MELT_19,
+	TILE_MELT_20,
+	TILE_MELT_21,
+	TILE_MELT_22,
+	TILE_MELT_23,
+	TILE_MELT_24,
+	TILE_MELT_25,
+	TILE_MELT_26,
+	TILE_MELT_27,
+	TILE_MELT_28,
+	TILE_MELT_29,
+	TILE_MELT_30,
+	TILE_MELT_31,
+	TILE_MELT_32,
+	TILE_MELT_33,
+	TILE_MELT_34,
+	TILE_MELT_35,
+	TILE_MELT_36,
+	TILE_MELT_37,
+	TILE_MELT_38,
+	TILE_MELT_39,
+	TILE_MELT_40,
+	TILE_MELT_41,
+	TILE_MELT_42,
+	TILE_MELT_43,
+	TILE_MELT_44,
+	TILE_MELT_45
 };
 
-/* TODO: Listar aquí los automátas */
+const char *images_names[NUM_IMAGES] = {
+	GAMEDATA_DIR "images/arcade.png",
+	GAMEDATA_DIR "images/down-1.png",
+	GAMEDATA_DIR "images/down-2.png",
+	GAMEDATA_DIR "images/down-3.png",
+	GAMEDATA_DIR "images/left-1.png",
+	GAMEDATA_DIR "images/left-2.png",
+	GAMEDATA_DIR "images/left-3.png",
+	GAMEDATA_DIR "images/right-1.png",
+	GAMEDATA_DIR "images/right-2.png",
+	GAMEDATA_DIR "images/right-3.png",
+	GAMEDATA_DIR "images/up-1.png",
+	GAMEDATA_DIR "images/up-2.png",
+	GAMEDATA_DIR "images/up-3.png"
+};
+
+#define IMG_TILE_NAME GAMEDATA_DIR "images/tiles.png"
+
+static int tiles_frames [] = {
+	1, /* 0 => 1 Portal normal */
+	2, 3, /* 1 => 2, 2 => 3 */
+	4, 5, /* 3 => 4, 4 => 5 */
+	6, 7, /* 5 => 6, 6 => 7 */
+	8, 9, /* 7 => 8, 8 => 9 */
+	10, 11, 12, /* 9 => 10, 10 => 11, 11 => 12 */
+	13, 14, 15, /* 12 => 13, 13 => 14, 14 => 15 */
+	16, 17, 18, /* 15 => 16, 16 => 17, 17 => 18 */
+	19, 20, 1,  /* 18 => 19, 19 => 20, 20 => 1 */
+	
+	22, /* 21 => 22 Portal Rapido */
+	23, /* 22 => 23 */
+	24, /* 23 => 24 */
+	25, /* 24 => 25 */
+	22, /* 25 => 22 */
+	
+	27, /* 26 => 27 Llave */
+	28, 29, /* 27 => 28, 28 => 29 */
+	30, 31, /* 29 => 30, 30 => 31 */
+	32, 33, /* 31 => 32, 32 => 33 */
+	34, 35, /* 33 => 34, 34 => 35 */
+	36, 37, /* 35 => 36, 36 => 37 */
+	38, 39, /* 37 => 38, 38 => 39 */
+	40, 41, /* 39 => 40, 40 => 41 */
+	42, 43, /* 41 => 42, 42 => 43 */
+	44, 45, /* 43 => 44, 44 => 45 */
+	46, 47, /* 45 => 46, 46 => 47 */
+	48, 49, /* 47 => 48, 48 => 49 */
+	50, 51, /* 49 => 50, 50 => 51 */
+	52, 53, /* 51 => 52, 52 => 53 */
+	54, 55, /* 53 => 54, 54 => 55 */
+	56, 57, /* 55 => 56, 56 => 57 */
+	58, 27, /* 57 => 58, 58 => 27 */
+	
+	59, /* 59 => 59, Goal */
+	60, /* 60 => 60, Vacio */
+	61, /* 61 => 61, Hielo */
+	62, /* 62 => 62, Espacio para la caja */
+	63, /* 63 => 63, Hielo doble */
+	64, /* 64 => 64, Portal inactivo */
+	65, /* 65 => 65, Candado */
+	66, /* 66 => 66, Boton 1 */
+	67, /* 67 => 67, Boton utilizado */
+	68, /* 68 => 68, Muro */
+	69, /* 69 => 69, Bloque */
+	70, /* 70 => 70, Bolsa de dinero */
+	
+	/* Hielo derriendose, Agua moviendo */
+	72, 73, 74, 75,
+	76, 77, 78, 79, 80,
+	81, 82, 83, 84, 85,
+	86, 87, 88, 89, 90,
+	91, 92, 93, 94, 95,
+	96, 97, 98, 99, 100,
+	101, 102, 103, 104, 105,
+	106, 107, 108, 109, 110,
+	111, 112, 113, 114, 115,
+	116, 78,
+	
+	/* Torbellino */
+	118, 119, 120, 121, 122,
+	123, 124, 125, 126, 127, 128, 129, 130,
+	131, 132, 133, 134, 135, 136, 137, 119
+};
+
+
+static int tiles_outputs [] = {
+	TILE_PORTAL_1,
+	TILE_PORTAL_1, TILE_PORTAL_1,
+	TILE_PORTAL_2, TILE_PORTAL_2,
+	TILE_PORTAL_3, TILE_PORTAL_3,
+	TILE_PORTAL_4, TILE_PORTAL_4,
+	TILE_PORTAL_1, TILE_PORTAL_1, TILE_PORTAL_1,
+	TILE_PORTAL_2, TILE_PORTAL_2, TILE_PORTAL_2,
+	TILE_PORTAL_3, TILE_PORTAL_3, TILE_PORTAL_3,
+	TILE_PORTAL_4, TILE_PORTAL_4, TILE_PORTAL_4,
+	
+	TILE_PORTAL_5,
+	TILE_PORTAL_5,
+	TILE_PORTAL_6,
+	TILE_PORTAL_7,
+	TILE_PORTAL_8,
+	
+	TILE_KEY_1,
+	TILE_KEY_1, TILE_KEY_1,
+	TILE_KEY_2, TILE_KEY_2,
+	TILE_KEY_3, TILE_KEY_3,
+	TILE_KEY_4, TILE_KEY_4,
+	TILE_KEY_5, TILE_KEY_5,
+	TILE_KEY_6, TILE_KEY_6,
+	TILE_KEY_7, TILE_KEY_7,
+	TILE_KEY_8, TILE_KEY_8,
+	TILE_KEY_9, TILE_KEY_9,
+	TILE_KEY_10, TILE_KEY_10,
+	TILE_KEY_11, TILE_KEY_11,
+	TILE_KEY_12, TILE_KEY_12,
+	TILE_KEY_13, TILE_KEY_13,
+	TILE_KEY_14, TILE_KEY_14,
+	TILE_KEY_15, TILE_KEY_15,
+	TILE_KEY_16, TILE_KEY_16,
+	
+	TILE_GOAL,
+	TILE_EMPTY,
+	TILE_ICE,
+	TILE_BOXSPACE,
+	TILE_DOBLE,
+	TILE_PORTAL_EMPTY,
+	TILE_LOCK,
+	TILE_SWITCH_A,
+	TILE_SWITCH_B,
+	TILE_WALL,
+	TILE_BLOCK,
+	TILE_BAG,
+	
+	TILE_MELT_1,
+	TILE_MELT_1, TILE_MELT_2, TILE_MELT_3, TILE_MELT_4, TILE_MELT_5,
+	TILE_MELT_6, TILE_MELT_7, TILE_MELT_8, TILE_MELT_9, TILE_MELT_10,
+	TILE_MELT_11, TILE_MELT_12, TILE_MELT_13, TILE_MELT_14, TILE_MELT_15,
+	TILE_MELT_16, TILE_MELT_17, TILE_MELT_18, TILE_MELT_19, TILE_MELT_20,
+	TILE_MELT_21, TILE_MELT_22, TILE_MELT_23, TILE_MELT_24, TILE_MELT_25,
+	TILE_MELT_26, TILE_MELT_27, TILE_MELT_28, TILE_MELT_29, TILE_MELT_30,
+	TILE_MELT_31, TILE_MELT_32, TILE_MELT_33, TILE_MELT_34, TILE_MELT_35,
+	TILE_MELT_36, TILE_MELT_37, TILE_MELT_38, TILE_MELT_39, TILE_MELT_40,
+	TILE_MELT_41, TILE_MELT_42, TILE_MELT_43, TILE_MELT_44, TILE_MELT_45,
+	
+	TILE_LOSE_1,
+	TILE_LOSE_1, TILE_LOSE_2, TILE_LOSE_3, TILE_LOSE_4,
+	TILE_LOSE_5, TILE_LOSE_6, TILE_LOSE_7, TILE_LOSE_8,
+	TILE_LOSE_9, TILE_LOSE_10, TILE_LOSE_11, TILE_LOSE_12,
+	TILE_LOSE_13, TILE_LOSE_14, TILE_LOSE_15, TILE_LOSE_16,
+	TILE_LOSE_17, TILE_LOSE_18, TILE_LOSE_19, TILE_LOSE_20
+};
+
+static int tiles_inicio [] = {
+	0,
+	60,
+	61,
+	0,
+	63,
+	59,
+	
+};
 
 /* Codigos de salida */
 enum {
@@ -79,9 +405,11 @@ enum {
 int game_loop (void);
 void setup (void);
 SDL_Surface * set_video_mode(unsigned);
+void copy_tile (SDL_Rect *rect, int tile);
 
 /* Variables globales */
 SDL_Surface * screen;
+SDL_Surface * image_tiles;
 SDL_Surface * images [NUM_IMAGES];
 
 int main (int argc, char *argv[]) {
@@ -102,9 +430,24 @@ int game_loop (void) {
 	SDLKey key;
 	Uint32 last_time, now_time;
 	SDL_Rect rect;
+	int portal_f = 0, f_rapido = 21;
+	int llave = 26;
+	int box[20];
+	
+	int g;
+	
+	for (g = 0; g < 12; g++) {
+		box[g] = g + 59;
+	}
+	
+	box[12] = 71; /* Melt */
+	box[13] = 117; /* Torbellino */
+	
+	Uint32 negro;
 	
 	SDL_EventState (SDL_MOUSEMOTION, SDL_IGNORE);
 	
+	negro = SDL_MapRGB (screen->format, 0, 0, 0);
 	do {
 		last_time = SDL_GetTicks ();
 		
@@ -114,10 +457,45 @@ int game_loop (void) {
 					/* Vamos a cerrar la aplicación */
 					done = GAME_QUIT;
 					break;
+				case SDL_KEYDOWN:
+					if (event.key.keysym.sym == SDLK_a) {
+						
+					} else if (event.key.keysym.sym == SDLK_b) {
+						
+					}
+					break;
 			}
 		}
 		
+		rect.y = 100;
 		
+		rect.x = 96;
+		portal_f = tiles_frames [portal_f];
+		copy_tile (&rect, tiles_outputs [portal_f]);
+		
+		rect.x = 120;
+		f_rapido = tiles_frames [f_rapido];
+		copy_tile (&rect, tiles_outputs [f_rapido]);
+		
+		rect.x = 144;
+		llave = tiles_frames [llave];
+		copy_tile (&rect, tiles_outputs [llave]);
+		
+		for (g = 0; g < 12; g++) {
+			rect.x = 168 + (g * 24);
+			box[g] = tiles_frames [box[g]];
+			copy_tile (&rect, tiles_outputs [box[g]]);
+		}
+		
+		rect.x = 456;
+		box[12] = tiles_frames [box[12]];
+		copy_tile (&rect, tiles_outputs [box[12]]);
+		
+		rect.x = 480;
+		box[13] = tiles_frames [box[13]];
+		copy_tile (&rect, tiles_outputs [box[13]]);
+		
+		//printf ("Melt: %i\n", box[12] - 72 + 1);
 		SDL_Flip (screen);
 		
 		now_time = SDL_GetTicks ();
@@ -180,6 +558,29 @@ void setup (void) {
 		/* TODO: Mostrar la carga de porcentaje */
 	}
 	
+	/* Cargar los tiles */
+	image_tiles = IMG_Load (IMG_TILE_NAME);
+	
+	if (image_tiles == NULL) {
+		fprintf (stderr,
+				"Failed to load data file:\n"
+				"%s\n"
+				"The error returned by SDL is:\n"
+				"%s\n", images_names[g], SDL_GetError());
+			SDL_Quit ();
+			exit (1);
+	}
+	
 	srand (SDL_GetTicks ());
 }
 
+inline void copy_tile (SDL_Rect *rect, int tile) {
+	SDL_Rect r_tile;
+	
+	rect->w = r_tile.w = TILE_WIDTH;
+	rect->h = r_tile.h = TILE_HEIGHT;
+	r_tile.x = (tile % 8) * TILE_WIDTH;
+	r_tile.y = (tile / 8) * TILE_HEIGHT;
+	
+	SDL_BlitSurface (image_tiles, &r_tile, screen, rect);
+}
